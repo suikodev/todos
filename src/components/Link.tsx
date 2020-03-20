b3d660406db12cda6515d27dad15d07e7ed1e604
Nacht <realnacht@outlook.com>
2020-03-21 03:15:58 +0800

    refactor

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -3,6 +3,7 @@ import * as React from "react";
 export const Link: React.FC<{
   active: boolean;
   onclick: () => void;
+  children: string;
 }> = props => {
   if (props.active) {
     return <span>{props.children}</span>;
